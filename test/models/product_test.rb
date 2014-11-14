55fc122fa21ffe2082aa447d4ec3e33f6a7601af
Max Haifei Li <max.li@live.com>
2014-11-14 12:54:12 -0600

    fixed the problem with testing. Fixture

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  #fixtures :Products 
 
 	test "product attributes must not be empty" do 
 	  product = Product.new
@@ -21,19 +22,46 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = -1
     assert product.invalid?
-    assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
+    assert_equal ["must be greater than or equal to 0.01"], 
+      product.errors[:price]
 
 
     product.price = 0
     assert product.invalid?
     assert_equal ["must be greater than or equal to 0.01"],
-    product.errors[:price]
+      product.errors[:price]
 
     product.price = 1
     assert product.valid?
-
-
 	end
+
+  def new_product(image_url)
+    Product.new(title:        "My Book Title",
+                description:  "yyy",
+                price:        1,
+                image_url:    image_url)
+  end
+  test 'image_url' do
+    ok = %w{fred.gif fred.jpg fred.png FRED.JPG FRED.jpg  
+            http://a.b.c/x/y/z/fred.gif}
+    bad = %w{fred.doc fred.gif/more fred.gif.more}
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title:  products(:ruby).title,
+                        description: "yyy", 
+                        price:        1,
+                        image_url:    "fred.gif")
+   assert product.invalid?
+   assert_equal ["      has already been taken"], product.errors[:title]
+  end
+
+
 
 end
